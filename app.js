84852a4068b0bc75d231db231f48c4c9d8ea0b85
leyosu23 <leyosu23@gmail.com>
2021-10-06 21:11:15 -0400

    implement built-in javascript method to return data, pass list of items in an array format and show data in a programmatic way

--- app.js
+++ app.js
@@ -2,44 +2,38 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+var items = ["Sleep", "Eat", "Exercise"];
 //app that's generated using express to use ejs as a view engine
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.get("/", function (req, res) {
     var today = new Date();
     var currentDay = today.getDay();
-    var day = "";
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-        default:
-            console.log("Err: not vaild day")
-            break;
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
     }
+
+    // tell javascript to set the dateString
+    var day = today.toLocaleDateString("en-US", options);
+
     res.render('list', {
-        kindOfDay: day
+        kindOfDay: day,
+        newListItems: items
     })
 });
+
+app.post("/", function (req, res) {
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+})
 
 app.listen(3000, function () {
     console.log("Server started on port 3000.");
