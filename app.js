f6e8bdc93d3bcb4b5d17e67a5bf7eeeb442805a0
leyosu23 <leyosu23@gmail.com>
2021-11-03 19:53:59 -0400

    Connect with mongoose

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 
 const app = express();
@@ -14,12 +14,43 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"))
 
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+    useNewUrlParser: true
+});
+
+const itemsSchema = {
+    name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+    name: "Welcome to your todolist!"
+});
+
+const item2 = new Item({
+    name: "Welcome to your todolist2!"
+});
+
+const item3 = new Item({
+    name: "Welcome to your todolist3!"
+});
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems, function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Successfully inserted!");
+    }
+})
+
 // default route
 app.get("/", function (req, res) {
 
-    let day = date.getDate();
     res.render('list', {
-        listTitle: day,
+        listTitle: "Today",
         newListItems: items
     })
 });
