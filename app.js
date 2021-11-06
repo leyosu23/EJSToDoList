857f15713256788cc67c1539bd84875f73293eb1
leyosu23 <leyosu23@gmail.com>
2021-11-05 20:33:14 -0400

    Connect MongoDB Atlas

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(
 );
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {
+mongoose.connect("mongodb+srv://admin:test123@cluster0.a4pvx.mongodb.net/todolistDB", {
   useNewUrlParser: true,
 });
 
